aeb068dc6031fdef1e9b894dfbfbe65c1e9a1011
strangecodder <strangecodder@gmail.com>
2024-05-14 19:23:18 +0300

    feat: добавил скрипт для очистки файлов с логами

---
+++ src/main/java/scrypt/clear.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -n > ../logFiles/emergency
+echo -n > ../logFiles/alerts
+echo -n > ../logFiles/critical
+echo -n > ../logFiles/errors
+echo -n > ../logFiles/warning
+echo -n > ../logFiles/notice
+echo -n > ../logFiles/info
+echo -n > ../logFiles/debug
+echo -n > ../logFiles/allTypes
