f7f7aa1346fc97a2c164f717acf850085e28b08b
strangecodder <strangecodder@gmail.com>
2024-05-16 15:55:18 +0300

    fix: убрал лишние зависимости

--- src/main/java/com/example/astrahakaton/HelloController.java
+++ src/main/java/com/example/astrahakaton/HelloController.java
@@ -4,17 +4,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
 
 
 
