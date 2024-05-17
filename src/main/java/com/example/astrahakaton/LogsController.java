21d50daca0e1a1e6f1f21e30c0ba9dea9458f8e8
mrskeletone <mr_skeletone_1000@mail.ru>
2024-05-17 20:02:59 +0300

    feat: добавлено создание графика и динамического меню

--- src/main/java/com/example/astrahakaton/LogsController.java
+++ src/main/java/com/example/astrahakaton/LogsController.java
@@ -6,12 +6,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LogsController {
@@ -19,18 +24,25 @@ public class LogsController {
     private TextArea textArea;
     @FXML
     private PieChart allLogsPie;
-    public void setPieData(Map<String,Long> data){
-        PieChart.Data[] pie=new PieChart.Data[data.size()];
-        int i=0;
+    @FXML
+    private MenuBar menuBar;
+
+    public void setPieData(Map<String, Long> data) {
+        PieChart.Data[] pie = new PieChart.Data[data.size()];
+        int i = 0;
         for (var entry :
                 data.entrySet()) {
-            pie[i]=new PieChart.Data(entry.getKey(),entry.getValue());
+            pie[i] = new PieChart.Data(entry.getKey(), entry.getValue());
             i++;
         }
         allLogsPie.setData(FXCollections.observableArrayList(pie));
     }
 
-    public void setTextArea(String textArea){
+    public void setMenuBar(Menu menu) {
+        menuBar.getMenus().add(menu);
+    }
+
+    public void setTextArea(String textArea) {
         this.textArea.setText(textArea);
     }
 
@@ -38,59 +50,96 @@ public class LogsController {
     protected void onClickBackButton() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        Stage stage=new HelloApplication().getPrimaryStage();
+        Stage stage = new HelloApplication().getPrimaryStage();
         stage.setScene(scene);
     }
+
     @FXML
-    protected void onClickMenuAlerts()throws IOException{
-        onClickMenu("alertsLogs-view.fxml","src/main/java/logFiles/alertsLogs/alerts");
+    protected void onClickMenuAlerts() throws IOException {
+        onClickMenu("alertsLogs-view.fxml", "src/main/java/logFiles/alertsLogs/alerts");
     }
+
     @FXML
-    protected void onClickMenuAllLogs()throws IOException{
-       onClickMenu("allLogs-view.fxml","src/main/java/logFiles/allTypesLogs/all_types");
+    protected void onClickMenuAllLogs() throws IOException {
+        onClickMenu("allLogs-view.fxml", "src/main/java/logFiles/allTypesLogs/all_types");
     }
+
     @FXML
-    protected void onClickMenuCrit()throws IOException{
-        onClickMenu("critLogs-view.fxml","src/main/java/logFiles/criticalLogs/critical");
+    protected void onClickMenuCrit() throws IOException {
+        onClickMenu("critLogs-view.fxml", "src/main/java/logFiles/criticalLogs/critical");
     }
+
     @FXML
-    protected void onClickMenuDebug()throws IOException{
-        onClickMenu("debugLogs-view.fxml","src/main/java/logFiles/debugLogs/debug");
+    protected void onClickMenuDebug() throws IOException {
+        onClickMenu("debugLogs-view.fxml", "src/main/java/logFiles/debugLogs/debug");
     }
+
     @FXML
-    protected void onClickMenuEmerg()throws IOException{
-        onClickMenu("emergLogs-view.fxml","src/main/java/logFiles/emergencyLogs/emergency");
+    protected void onClickMenuEmerg() throws IOException {
+        onClickMenu("emergLogs-view.fxml", "src/main/java/logFiles/emergencyLogs/emergency");
     }
+
     @FXML
-    protected void onClickMenuErrors()throws IOException{
-        onClickMenu("errorsLogs-view.fxml","src/main/java/logFiles/errorLogs/errors");
+    protected void onClickMenuErrors() throws IOException {
+        onClickMenu("errorsLogs-view.fxml", "src/main/java/logFiles/errorLogs/errors");
     }
+
     @FXML
-    protected void onClickMenuInfo()throws IOException{
-        onClickMenu("infoLogs-view.fxml","src/main/java/logFiles/infoLogs/info");
+    protected void onClickMenuInfo() throws IOException {
+        onClickMenu("infoLogs-view.fxml", "src/main/java/logFiles/infoLogs/info");
     }
+
     @FXML
-    protected void onClickMenuNotice()throws IOException{
-        onClickMenu("noticeLogs-view.fxml","src/main/java/logFiles/noticeLogs/notice");
+    protected void onClickMenuNotice() throws IOException {
+        onClickMenu("noticeLogs-view.fxml", "src/main/java/logFiles/noticeLogs/notice");
     }
+
     @FXML
-    protected void onClickMenuWarning()throws IOException{
-        onClickMenu("warningLogs-view.fxml","src/main/java/logFiles/warningLogs/warning");
+    protected void onClickMenuWarning() throws IOException {
+        onClickMenu("warningLogs-view.fxml", "src/main/java/logFiles/warningLogs/warning");
     }
+
     //метод для переключение между типами логов
-    private void onClickMenu(String fxml,String path) throws IOException{
+    private void onClickMenu(String fxml, String path) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fxml));
         Scene scene = new Scene(fxmlLoader.load());
-        Stage stage=new HelloApplication().getPrimaryStage();
+        Stage stage = new HelloApplication().getPrimaryStage();
         stage.setScene(scene);
-        StringBuilder s= new StringBuilder();
-        int i=0;
-        try(BufferedReader bufferedReader=new BufferedReader(new FileReader(path))){
-            while (bufferedReader.ready()){
-                s.append(bufferedReader.readLine()).append("\n");
+        StringBuilder s = new StringBuilder();
+        int i = 0;
+        Map<String, Long> data = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                s.append(line).append("\n");
+                if (!fxml.equals("allLogs-view.fxml")) {
+                    String[] arrLine = line.split(" ");
+                    int flag = arrLine[4].indexOf("[");
+                    if (flag < 0) {
+                        flag = arrLine[4].indexOf(":");
+                    }
+                    String subString = arrLine[4].substring(0, flag);
+                    if (!data.containsKey(subString)) {
+                        data.put(subString, 0L);
+                    } else {
+                        long l = data.get(subString);
+                        l++;
+                        data.put(subString, l);
+                    }
+                }
             }
         }
-        LogsController logsController= fxmlLoader.getController();
+
+        LogsController logsController = fxmlLoader.getController();
         logsController.setTextArea(String.valueOf(s));
+        if (!fxml.equals("allLogs-view.fxml")) {
+            Menu menu=new Menu("Фильтровать по");
+            for (var key :
+                    data.keySet()) {
+                    menu.getItems().add(new MenuItem(key));
+            }
+            logsController.setMenuBar(menu);
+            logsController.setPieData(data);
+        }
     }
 }
