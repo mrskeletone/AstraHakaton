b808f0f514e55fac5616498618aa9f85bb6ee39a
mrskeletone <mr_skeletone_1000@mail.ru>
2024-05-20 21:17:40 +0300

    feat: добавил методы для оптимизации кода
    getType получение типа лога
    
    stringToLogs перевод string в Logs
    
    сreateLogsTable создание таблицы с настроенными калонками и размером

--- src/main/java/com/example/astrahakaton/Util.java
+++ src/main/java/com/example/astrahakaton/Util.java
@@ -1,12 +1,55 @@
 package com.example.astrahakaton;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.io.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Util {
     private Util(){
     }
+    public static String getType(String s){
+        int flag = s.indexOf("[");
+        if (flag < 0) {
+            flag = s.indexOf(":");
+        }
+        return s.substring(0, flag);
+    }
+    public static Logs stringToLogs(String line){
+        StringBuilder s = new StringBuilder();
+        String[] split = line.split(" ");
+        for (int j = 5; j < split.length; j++) {
+            s.append(" ").append(split[j]);
+        }
+        String[] arrLine = line.split(" ");
+        String subString = Util.getType(arrLine[4]);
+        return  new Logs(split[0] + " " + split[1] + " " + split[2], split[3], subString, s.toString());
+    }
+  public static TableView<Logs> createLogsTable(ObservableList<Logs> logs){
+      TableView<Logs> table=new TableView<>(logs);
+      TableColumn<Logs, String> dateColumn = new TableColumn<>("Date");
+      dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+      table.getColumns().add(dateColumn);
+      TableColumn<Logs, String> userColumn = new TableColumn<>("User");
+      userColumn.setCellValueFactory(new PropertyValueFactory<>("user"));
+      table.getColumns().add(userColumn);
+      TableColumn<Logs, String> typeColumn = new TableColumn<>("Type");
+      typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+      table.getColumns().add(typeColumn);
+      TableColumn<Logs, String> commentColumn = new TableColumn<>("Comment");
+      commentColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
+      table.getColumns().add(commentColumn);
+
+      table.setPrefSize(1000,500);
+      return table;
+
+  }
    static long lineNumber(String path){
        long noOfLines = -1;
 
